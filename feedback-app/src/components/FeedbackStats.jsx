4e469a2edb4a6f37283177b90f65a9471c991792
boseaditya21 <aditya@cloudkaptan.com>
2022-02-15 22:22:03 +0530

    Netlify, JSON Server, and Concurrently

--- feedback-app/src/components/FeedbackStats.jsx
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import {useContext} from 'react';
+import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackStats({  }) {
-  const {feedback} = useContext(FeedbackContext)
-  //Calculate ratings average
-  let average =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating;
-    }, 0) / feedback.length;
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
 
-  average = average.toFixed(1).replace(/[.,]0$/, "");
+  // NOTE: simpler average calculation
+  // Calculate ratings avg
+  const average = Math.round(
+    feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
+  );
+
+  // average = average.toFixed(1).replace(/[.,]0$/, '')
+  // average = Math.round(average)
 
   return (
     <div className="feedback-stats">
