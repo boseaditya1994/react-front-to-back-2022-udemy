c141c924efbeb51f62b13b0e6045a6c613030606
boseaditya21 <aditya@cloudkaptan.com>
2022-02-23 23:42:41 +0530

    Github Finder Project Completion

--- github-finder/src/components/repos/RepoItem.jsx
+++ github-finder/src/components/repos/RepoItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FaLink, FaEye, FaStar, FaInfo, FaUtensils } from "react-icons/fa";
 
 function RepoItem({ repo }) {
   const {
@@ -11,8 +12,33 @@ function RepoItem({ repo }) {
     watchers_count,
     stargazers_count,
   } = repo;
-  
-  return <div>Item</div>;
+
+  return (
+    <div className="mb-2 rounded-md card bg-grey-800 hover:bg-grey-900">
+      <div className="card-body">
+        <h3 className="mb-2 text-xl font-semibold">
+          <a href={html_url}>
+            <FaLink className="inline mr-1"></FaLink> {name}
+          </a>
+        </h3>
+        <p className="mb-3">{description}</p>
+        <div>
+          <div className="mr-2 badge badge-info badge-lg">
+            <FaEye className="mr-2">{watchers_count}</FaEye>
+          </div>
+          <div className="mr-2 badge badge-success badge-lg">
+            <FaStar className="mr-2">{stargazers_count}</FaStar>
+          </div>
+          <div className="mr-2 badge badge-error badge-lg">
+            <FaInfo className="mr-2">{open_issues}</FaInfo>
+          </div>
+          <div className="mr-2 badge badge-warning badge-lg">
+            <FaUtensils className="mr-2">{watchers_count}</FaUtensils>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 RepoItem.propTypes = {
